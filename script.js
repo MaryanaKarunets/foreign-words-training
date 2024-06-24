d256120462bb81faee54b55ddb0208656d7441b0
Maryana Karunets <MaryanaKarunets@MacBook-Air-Maryana.local>
2024-06-24 23:46:28 +0300

    Update script.js
    массив слов для перевода

---
+++ script.js
@@ -0,0 +1,30 @@
+const words = [{
+        word: 'cat',
+        translate: 'кот',
+        example: 'The cat likes milk.'
+    },
+
+    {
+        word: 'dog',
+        translate: 'собака',
+        example: 'The dog guards the house.'
+    },
+
+    {
+        word: 'parrot',
+        translate: 'попугай',
+        example: 'The parrot can repeat words.'
+    },
+
+    {
+        word: 'hamster',
+        translate: 'хомяк',
+        example: 'The hamster likes to eat grains.'
+    },
+
+    {
+        word: 'monkey',
+        translate: 'обезьянка',
+        example: 'The monkey likes to eat bananas.'
+    }
+]
